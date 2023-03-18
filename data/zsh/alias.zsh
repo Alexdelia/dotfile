4515ec61b872b7c9a1d8cd0e5549e37904a3697f
Alexdelia <alexandre.delille.57@gmail.com>
2023-03-18 13:19:52 +0100

    fix

--- data/zsh/alias.zsh
+++ data/zsh/alias.zsh
@@ -1,7 +1,6 @@
 # alias	norminette="~/.norminette/norminette.rb"
 # alias	norminette_v2="~/.norminette_old/norminette.rb"
 alias	nor="norminette"
-alias	py42="~/goinfre/$USER/miniconda3"
 
 # alias	gcc="clang-9"
 # alias	clang="clang-9"
@@ -46,7 +45,30 @@ alias	w2x=/home/alex/Ware/waifu2x-ncnn-vulkan-20210521-ubuntu/waifu2x-ncnn-vulka
 alias	auto_w2x=/home/alex/p/bin/utility/auto_waifu2x.sh
 alias	aw2x=/home/alex/p/bin/utility/auto_waifu2x.sh
 alias	ani-cli=/home/alex/Ware/ani-cli/ani-cli
-alias	yuzu=/home/alex/Games/Yuzu/yuzu-20220305-ed691f09c.AppImagelias	up="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
+alias	yuzu=/home/alex/Games/Yuzu/yuzu-20220305-ed691f09c.AppImagealias	nor="norminette"
+
+# alias	gcc="clang-9"
+# alias	clang="clang-9"
+alias	clangW="clang -Wall -Werror -Wextra"
+
+alias	ga="git add --all && git status --short"
+alias 	gc="git commit --message"
+alias	gst="git status --short"
+alias	gtree="git log --graph --oneline --decorate"
+
+alias	sl="ls"
+alias	mk="make"
+alias	amke="make"
+alias	mkae="make"
+alias	maek="make"
+alias	ivm="vim"
+alias	imv="vim"
+alias	gti="git"
+
+alias	c="cargo"
+alias	s="sudo"
+
+alias	up="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
 alias	dlist="echo -e '\n\t\033[32;1mcontainer\033[0m' && \
 docker ps -a && \
 echo -e '\n\t\033[32;1mimage\033[0m' && \
