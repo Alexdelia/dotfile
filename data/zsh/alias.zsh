b9dfb9335fbd5ff59efc34ae1c6fdb8035b26ddd
Alexdelia <alexandre.delille.57@gmail.com>
2023-03-19 17:08:03 +0100

    `cg-local`

--- data/zsh/alias.zsh
+++ data/zsh/alias.zsh
@@ -38,6 +38,7 @@ alias	neo="neofetch"
 alias	to="bashtop"
 alias	poke="pokemon-colorscripts -r"
 
+alias	cg-local="java -jar ~/Ware/cg-local-app-1.3.0.jar"
 alias	rmbk=~/p/bin/utility/rmbk.sh
 # alias	w2x=/var/lib/flatpak/app/com.github.nihui.waifu2x-ncnn-vulkan/current/8bf9ef4885a0ca7426344fd1a080f8290c42373bb3e7aa5327a189c9319b4a27/export/bin/com.github.nihui.waifu2x-ncnn-vulkan
 alias	waifu2x=/home/alex/Ware/waifu2x-ncnn-vulkan-20210521-ubuntu/waifu2x-ncnn-vulkan
