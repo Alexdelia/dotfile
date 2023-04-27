a9a850fd71ecdf5b9e45e9b51f35c25df8c51ba3
Alexdelia <alexandre.delille.57@gmail.com>
2023-04-27 11:39:33 +0200

    rmbk

--- data/zsh/function.zsh
+++ data/zsh/function.zsh
@@ -38,7 +38,7 @@ function gpn() {
 		return 1
 	fi
 
-	commit=""
+	local commit=""
 
 	for file in "$@"; do
 		git add "$file"
@@ -49,25 +49,38 @@ function gpn() {
 	git push --quiet
 }
 
-# function rmbk() {
-# 	if [[ $# -lt 1 ]]; then
-# 		find . -type d \( -name '.git*' -o -name 'node_modules'\) -prune -o -name '*~' -delete
-# 		echo -e "\e[32;1mdone\e[0m"
-# 	else
-# 		HELP="\033[1m$0 \033[35m[option]\033[0m\n\noption:\n\t\033[1m-h, --help\033[0m\tshow this help message and exit"
-# 		CMD='find . -type d \( -name ".git*" \) -prune -o -name "*~"'
+function rmbk() {
+	local CMD='find . -type d \( -name ".git*" -o -path "./target" -o -name "node_modules" \) -prune -o -type f -name "*~"'
 
-# 		if [[ $1 == "-h" || $1 == "--help" ]]; then
-# 			echo -e $HELP
-# 			return 0
-# 		elif [[ $1 == "-p" || $1 == "--print" ]]; then
-# 			eval $CMD -print
-# 		elif [[ $1 == "-s" || $1 == "--show" ]]; then
-# 			eval $CMD -delete -print
-# 		else
-# 			echo -e "unknown option:\t\033[1;33m$1\033[0m"
-# 			echo -e $HELP
-# 			return 1
-# 		fi
-# 	fi
-# }
+	if [[ $# -lt 1 ]]; then
+		local start="$(date -u +%s.%N)"
+
+		local n="$(eval $CMD -print -exec rm -f {} + | wc -l)"
+
+		local end="$(date -u +%s.%N)"
+		local elapsed="$((end - start))"
+
+		printf "\033[1;32m$n\033[0m in \033[1;32m%.0f\033[0m \033[32mms\033[0m\n" "$((elapsed * 1000))"
+
+		return 0
+	fi
+
+	local HELP="usage: \033[1m$0 \033[35m[option]\033[0m
+option:
+	\033[1m-h, --help\033[0m\tshow this help message and exit
+	\033[1m-p, --print\033[0m\tprint found files
+	\033[1m-s, --show\033[0m\tdelete found files and print them"
+
+	if [[ $1 == "-h" || $1 == "--help" ]]; then
+		echo -e $HELP
+		return 0
+	elif [[ $1 == "-p" || $1 == "--print" ]]; then
+		eval $CMD -print
+	elif [[ $1 == "-s" || $1 == "--show" ]]; then
+		eval $CMD -print -exec rm -f {} +
+	else
+		echo -e "unknown option:\t\033[1;33m$1\033[0m"
+		echo -e $HELP
+		return 1
+	fi
+}
