5d359e0d3e9362fdbd982341af548d4c23345f9e
Alexdelia <alexandre.delille.57@gmail.com>
2023-04-28 11:19:04 +0200

    fix non utf8 diff

--- src/process/handle/file.rs
+++ src/process/handle/file.rs
@@ -4,6 +4,9 @@ use ansi::abbrev::D;
 use ansi::{B_BLUE, B_RED};
 use ux::{ask, ask_yn, AskKey};
 
+use std::fs::File;
+use std::io::{BufReader, Read};
+
 pub fn handle_file(symlink: &Symlink, interactive: bool) -> std::io::Result<()> {
     println!(
         "found already existing file:
@@ -35,23 +38,56 @@ pub fn handle_file(symlink: &Symlink, interactive: bool) -> std::io::Result<()> 
     symlink.create()
 }
 
-fn diff(symlink: &Symlink, interactive: bool) -> std::io::Result<bool> {
-    let original = std::fs::read_to_string(&symlink.target)?;
-    let modified = std::fs::read_to_string(&symlink.path)?;
-    let patch = diffy::create_patch(&original, &modified);
+#[derive(PartialEq)]
+enum Diff<'a, T: ToOwned + ?Sized> {
+    Utf8(diffy::Patch<'a, T>),
+    NonUtf8,
+    Identical,
+}
 
-    let c = if patch.hunks().is_empty() {
+fn diff(symlink: &Symlink, interactive: bool) -> std::io::Result<bool> {
+    let original = std::fs::read_to_string(&symlink.target);
+    let modified = std::fs::read_to_string(&symlink.path);
+
+    let diff: Diff<str> = match (&original, &modified) {
+        (Ok(original), Ok(modified)) => {
+            let patch = diffy::create_patch(original, modified);
+            if patch.hunks().is_empty() {
+                Diff::Identical
+            } else {
+                Diff::Utf8(patch)
+            }
+        }
+        _ => {
+            if diff_non_utf8(symlink)? {
+                Diff::Identical
+            } else {
+                Diff::NonUtf8
+            }
+        }
+    };
+
+    let c: char = if diff == Diff::Identical {
         println!("both files are identical");
-        'k'
+        Some('k')
     } else if !interactive {
-        'I'
-    } else {
+        Some('I')
+    } else if let Diff::Utf8(patch) = diff {
         println!(
             "diff:\n--- {B_RED}{system}{D}\n+++ {B_BLUE}{data}{D}\n{patch}\n",
             data = symlink.target.display(),
+            system = symlink.path.display(),
+        );
+        None
+    } else {
+        println!(
+            "{B_RED}{system}{D} and {B_BLUE}{data}{D} are not identical",
+            data = symlink.target.display(),
             system = symlink.path.display()
         );
-
+        None
+    }
+    .unwrap_or_else(|| {
         ask(
             &format!(
                 "what should be done on {M}{path}{D}:
@@ -66,7 +102,7 @@ fn diff(symlink: &Symlink, interactive: bool) -> std::io::Result<bool> {
             None,
         )
         .unwrap() // panic on purpose
-    };
+    });
 
     match c {
         'k' => symlink.print_action("keeped", Some(B_BLUE)),
@@ -79,6 +115,26 @@ fn diff(symlink: &Symlink, interactive: bool) -> std::io::Result<bool> {
             symlink.print_action("replaced", Some(B_RED));
         }
         _ => unreachable!("diff() should find char: k, r or I"),
+    }
+
+    Ok(true)
+}
+
+fn diff_non_utf8(symlink: &Symlink) -> std::io::Result<bool> {
+    let original = File::open(&symlink.target)?;
+    let modified = File::open(&symlink.path)?;
+
+    if original.metadata()?.len() != modified.metadata()?.len() {
+        return Ok(false);
+    }
+
+    let original = BufReader::new(original);
+    let modified = BufReader::new(modified);
+
+    for (b1, b2) in original.bytes().zip(modified.bytes()) {
+        if b1? != b2? {
+            return Ok(false);
+        }
     }
 
     Ok(true)
