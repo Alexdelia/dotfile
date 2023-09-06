9a61a606c38c0d46c8559676d0965f5755ee621d
adelille <adelille@student.42.fr>
2023-09-06 12:08:14 +0200

    default git editor to vim

--- data/zsh/export.zsh
+++ data/zsh/export.zsh
@@ -3,4 +3,6 @@ if [[ ! -z "$P_WARE" ]]; then
 	export PATH="$P_WARE/utility:$PATH"
 fi
 
-export ANDROID_NDK=/home/alex/Android/Sdk/ndk
+export GIT_EDITOR="vim"
+
+export ANDROID_NDK="/home/alex/Android/Sdk/ndk"
