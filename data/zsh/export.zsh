d19ce383f594cb1805b120c2923be24ac79ae206
adelille <adelille@student.42.fr>
2023-09-21 11:17:52 +0200

    ZSHCOMP

--- data/zsh/export.zsh
+++ data/zsh/export.zsh
@@ -3,6 +3,8 @@ if [[ ! -z "$P_WARE" ]]; then
 	export PATH="$P_WARE/utility:$PATH"
 fi
 
+export ZSH_COMPDUMP="$ZSH/cache/zcompdump-$HOST-$ZSH_VERSION"
+
 export GIT_EDITOR="vim"
 
 export MAIL="adelille@student.42.fr"
