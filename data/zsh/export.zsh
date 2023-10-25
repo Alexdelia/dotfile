218f4b62a5e3c212e9f4350617b1123dbc97b51f
Alexdelia <alexandre.delille.57@gmail.com>
2023-10-25 20:03:17 +0200

    flutter settings

--- data/zsh/export.zsh
+++ data/zsh/export.zsh
@@ -1,6 +1,10 @@
 if [[ ! -z "$P_WARE" ]]; then
 	export PATH="$P_WARE:$PATH"
 	export PATH="$P_WARE/utility:$PATH"
+
+	if [[ ! -z "$P_WARE/flutter/bin" ]]; then
+		export PATH="$P_WARE/flutter/bin:$PATH"
+	fi
 fi
 
 export ZSH_COMPDUMP="$ZSH/cache/zcompdump-$HOST-$ZSH_VERSION"
@@ -9,4 +13,4 @@ export GIT_EDITOR="vim"
 
 export MAIL="adelille@student.42.fr"
 
-export ANDROID_NDK="/home/alex/Android/Sdk/ndk"
+export ANDROID_NDK="$HOME/Android/Sdk/ndk"
