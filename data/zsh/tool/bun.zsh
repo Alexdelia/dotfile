6bda58458f30a0ca99f1e878af72ab215b50cd4d
Alexdelia <alexandre.delille.57@gmail.com>
2024-01-10 12:33:53 +0100

    tool

---
+++ data/zsh/tool/bun.zsh
@@ -0,0 +1,9 @@
+if ((!$ + commands[bun])); then
+	return
+fi
+
+export BUN_INSTALL="$P_TOOL/bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# bun completions
+[ -s "${BUN_INSTALL}/_bun" ] && source "${BUN_INSTALL}/_bun"
---
+++ data/zsh/tool/main.zsh
@@ -0,0 +1,8 @@
+P_TOOL="$HOME/.w"
+
+# asdf
+ASDF_DATA_DIR="$P_TOOL/asdf"
+ASDF_DIR="$ASDF_DATA_DIR"
+
+# bun
+source "bun.zsh"
