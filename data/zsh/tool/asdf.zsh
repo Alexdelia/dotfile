1818b8bd5d4561b809a85a6725605d9203559da5
Alexdelia <alexandre.delille.57@gmail.com>
2024-01-11 18:39:39 +0100

    asdf move

--- data/zsh/tool/asdf.zsh
+++ data/zsh/tool/asdf.zsh
@@ -1,12 +1,12 @@
-# if asdf is still in ~/.asdf, print a warning
-if [[ -d "$HOME/.asdf" ]]; then
-	printf "$ZSH_ERR\033[1;35masdf\033[0m \033[1;33mstill in \033[1;31m~/.asdf\033[0m\n"
-	return
-fi
-
 export ASDF_DATA_DIR="$P_TOOL/asdf"
 export ASDF_DIR="$ASDF_DATA_DIR"
 export ASDF_CONFIG_FILE="$ASDF_DATA_DIR/asdfrc"
+
+if [[ -d "$HOME/.asdf" ]]; then
+	printf "$ZSH_INFO\033[1;36mmoving \033[1;35masdf \033[1;36mfrom \033[1;33m~/.asdf \033[1;36mto \033[1;31m$ASDF_DATA_DIR\033[0m\n"
+
+	mv "$HOME/.asdf" "$ASDF_DATA_DIR"
+fi
 
 if [[ ! -f "$ASDF_CONFIG_FILE" ]]; then
 	printf "legacy_version_file = yes\n" >"$ASDF_CONFIG_FILE"
