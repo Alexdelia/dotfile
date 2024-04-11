17d41b1b76d0d388187caa12393a1b4742998848
Alexdelia <alexandre.delille.57@gmail.com>
2024-04-11 10:06:39 +0200

    `~/.local/bin` to `PATH`

--- data/zsh/export.zsh
+++ data/zsh/export.zsh
@@ -11,6 +11,8 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
+export PATH="$HOME/.local/bin:$PATH"
+
 export ZSH_COMPDUMP="$ZSH/cache/zcompdump-$HOST-$ZSH_VERSION"
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
 
