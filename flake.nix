dca6e843c9fdcd3ef0980bd327834eaeb9662358
Alexdelia <alexandre.delille.57@gmail.com>
2024-05-25 17:30:15 +0200

    working `nix` `flake` configuration with `home-manager`

--- flake.nix
+++ flake.nix
@@ -3,25 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   outputs = {
     nixpkgs,
+
     home-manager,
     ...
   }: let
     system = "aarch64-linux";
     # system = "x86_64-linux";
+
     pkgs = nixpkgs.legacyPackages.${system};
+    # pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
   in {
     homeConfigurations = {
       alex = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
-          ./home-manager/home.nix
+          ./home.nix
         ];
       };
     };
